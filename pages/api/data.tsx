4d053d73aaf9ca2f9f8942fb6a112c6dd22cba4e
xiikii <andy864940012@gmail.com>
2022-04-06 02:12:54 +0800

    初始化项目

---
+++ pages/api/data.tsx
@@ -0,0 +1,10 @@
+const Data = [
+    {
+        "id": "1",
+        "name": "John Doe",
+        "image": "https://randomuser.me/api/portraits/",
+        "src": "https://google.com"
+    }
+]
+
+export default Data
