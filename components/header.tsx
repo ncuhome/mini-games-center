41f39f1f92be6fa09567b5fdba6402386d812841
xiikii <andy864940012@gmail.com>
2022-04-06 17:58:03 +0800

    新增了一个 小游戏中心 icon

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,21 @@
 import { Button } from "@geist-ui/core"
 import Image from "next/image"
 import addicon from '../public/addicon.svg'
+import centericon from '../public/center_icon.svg'
 
 const Header = () => {
     return (
         <div>
+            <div style={{ marginTop: "36px" }}>
+                <Image src={centericon} alt="center_icon" width="64px" height="64px" />
+            </div>
             <div
                 style={{
                     color: "white",
                     fontSize: "42px",
                     fontWeight: "800",
                     lineHeight: "56px",
-                    marginTop: "40px",
+                    marginTop: "4px",
                 }}>小游戏<br />中心</div>
             <div style={{ fontSize: "13px", color: "#919190", marginTop: "8px" }}>
                 汇聚有趣好玩手游的小游戏分享社区，足不出户与全校玩家共同交流并体验各类小游戏。<br />
@@ -19,7 +23,7 @@ const Header = () => {
             </div>
 
             <div style={{ marginTop: "24px" }}>
-                <Image src={addicon} alt="Addicon" width="44px" height="44px" onClick={() => window.location.href = "https://www.wenjuan.com/s/UZBZJvEOdvM/#%E3%80%8A%E5%B0%8F%E6%B8%B8%E6%88%8F%E6%8E%A8%E8%8D%90%E3%80%8B"} />
+                <Image src={addicon} alt="add_icon" width="44px" height="44px" onClick={() => window.location.href = "https://www.wenjuan.com/s/UZBZJvEOdvM/#%E3%80%8A%E5%B0%8F%E6%B8%B8%E6%88%8F%E6%8E%A8%E8%8D%90%E3%80%8B"} />
             </div>
         </div>
     )
