1bad5eebfbd551444345c66e636bde10929d9afd
xiikii <andy864940012@gmail.com>
2022-04-06 17:31:55 +0800

    点击 NCHUHOME 可跳转至主站

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,9 @@ const Footer = () => {
             textAlign: "center",
             color: "#919190",
             fontSize: "13px"
-        }}>
+        }}
+            onClick={() => window.location.href = "https://team.ncuos.com/"}
+        >
             @ 2022 NCUHOME <br />
         </div>
     )
