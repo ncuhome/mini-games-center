31503a79ccd0ff112c194184e52cf071de9358f9
xiikii <andy864940012@gmail.com>
2022-04-06 16:33:59 +0800

    将游戏作为备注上传在 data.tsx

--- pages/api/data.tsx
+++ pages/api/data.tsx
@@ -1,23 +1,87 @@
+// 🟡召唤神龙http://www.wesane.com/game/910/
+// 🟡小鸭子过河http://www.wesane.com/game/883/
+// 🟡蝶恋花http://www.wesane.com/game/884/
+// 🟡打碎蔬菜http://www.wesane.com/game/892/
+// 🟡凿壁借光http://www.wesane.com/game/893/
+// 🟡插花瓣http://www.wesane.com/game/854/
+// 🟡山楂串http://www.wesane.com/game/848/
+// 🟡女王登基http://www.wesane.com/game/874/
+// 🟡龟龟滑雪http://www.wesane.com/game/793/
+// 🟡母鸡下蛋http://www.wesane.com/game/705/
+// 🟡合成大南瓜http://www.wesane.com/game/795/
+// 🟡解救公主http://www.wesane.com/game/782/
+// 🟡蛇棋http://www.wesane.com/game/791/
+// 🟡疯狂赛道http://www.wesane.com/game/869/
+// 🟡大鱼http://www.wesane.com/game/811/
+// 🟡花朵大作战http://www.wesane.com/game/739/
+// 🟡水果炸弹http://www.wesane.com/game/897/
+// 🟡抓螃蟹http://www.wesane.com/game/862/
+// 🟡旅行气球http://www.wesane.com/game/827/
+// 🟡小鱼找对象http://www.wesane.com/game/849/
+// 🟡刮腿毛 http://www.wesane.com/game/264/
+// 🟡打雪仗http://www.wesane.com/game/847/
+// 🟡建造通天塔http://www.wesane.com/game/816/[/cp]
+
+// http://www.wesane.com/game/793/ 龟龟滑雪（根本就不是人玩的）
+// http://www.wesane.com/game/795/ 合成大南瓜（太难了 ）
+// http://www.wesane.com/game/758/ 上菜（so easy）
+// http://www.wesane.com/game/781/ 串串香（so easy）
+// http://www.wesane.com/game/631/ 睡莲（有点无聊）
+// http://www.wesane.com/game/351/ 哥俩好（太难了 b的）
+// http://www.wesane.com/game/705/ 母鸡下蛋（so easy）
+// http://www.wesane.com/game/761/ 掏耳朵（吃饭别玩）
+// http://www.wesane.com/game/769/ 沉重打鸡（so easy）
+// http://www.wesane.com/game/747/ 刀削面（so easy）
+// http://www.wesane.com/game/596/ 刀工大师（敏捷度）
+// http://www.wesane.com/game/538/ 大甜筒（太难了）
+// http://www.wesane.com/game/264/ 刮腿毛（吃饭别玩）
+// http://www.wesane.com/game/81/?dt_dapp=1&dt_dapp=1 滴水不漏 （垃圾）
+// http://www.wesane.com/game/88/ 卖油翁（眼都怼瞎了）
+// http://www.wesane.com/game/684/ 车水马龙（有点难）
+// http://www.wesane.com/game/735/ 交换数字（有点难）
+// http://www.wesane.com/game/635/ 满汉全席（巨难啊艹）
+// http://www.wesane.com/game/525/ 奔跑的鱼丸（太难了靠）
+// http://www.wesane.com/game/757/ 水果碎碎冰（有点难）
+// http://www.wesane.com/game/263/ 花田（哈哈我好高）
+// http://www.wesane.com/game/470/ 汉堡跳跃（b的，好烦）
+// http://www.wesane.com/game/687/ 口红翻跟头（超级难）
+// http://www.wesane.com/game/717/ 跳高高（还是觉得难）
+// http://www.wesane.com/game/714/ 削掉它的皮（有点难）
+// http://www.wesane.com/game/565/ 植发
+// http://g.vsane.com/game/795/ 召唤神龙
+// 刮腿毛 http://www.wesane.com/game/264/
+// 彩虹屁 http://www.wesane.com/game/260/
+// 乌鸦喝水 http://g.fromgame.com/game/3/
+// 擦一擦 http://www.wesane.com/game/219/
+// 翻转水瓶 http://www.wesane.com/game/268/
+// 磁铁粘粘 http://www.wesane.com/game/270/
+// 旋转飞刀 http://www.wesane.com/game/358/
+// 守护蛋蛋 http://www.wesane.com/game/262/
+// 无敌破坏球 http://www.wesane.com/game/179/
+// 打泡泡 http://www.wesane.com/Public/Games/326029/201512100736543705/html5/index.html
+// 纸牌接龙 http://www.wesane.com/Public/Games/326029/201707050358126729/html5/index.html
+// 切水果 http://www.wesane.com/game/215/
+// 搭桥 http://www.wesane.com/game/89/
+
+
 
 const Data = [
     {
         "id": "1",
         "name": "召唤神龙",
         "src": "http://www.wesane.com/game/910/",
-        "image": "http://rongcloud-web.qiniudn.com/docs_demo_rongcloud_logo.png",
     },
     {
         "id": "2",
         "name": "小鸭子过河",
         "src": "http://www.wesane.com/game/883/",
-        "image": "http://rongcloud-web.qiniudn.com/docs_demo_rongcloud_logo.png",
     },
     {
         "id": "3",
         "name": "蝶恋花",
         "src": "http://www.wesane.com/game/884/",
-        "image": "http://rongcloud-web.qiniudn.com/docs_demo_rongcloud_logo.png",
-    }
+    },
+
 ]
 
 export default Data
