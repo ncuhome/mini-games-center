eea382d97232b6ad6b989f3b1aa1a06d73d6bfdb
xiikii <andy864940012@gmail.com>
2022-04-15 14:56:19 +0800

    显示今日新增的游戏数量

--- components/header.tsx
+++ components/header.tsx
@@ -22,8 +22,9 @@ const Header = () => {
                 休闲、解压、益智....丰富类别等你体验，每一款推荐的游戏都由我们为你精选而出，只为拯救你的无聊与烦闷！
             </div>
 
-            <div style={{ marginTop: "24px" }}>
-                <Image src={addicon} alt="add_icon" width="44px" height="44px" onClick={() => window.location.href = "https://www.wenjuan.com/s/UZBZJvEOdvM/#%E3%80%8A%E5%B0%8F%E6%B8%B8%E6%88%8F%E6%8E%A8%E8%8D%90%E3%80%8B"} />
+            <div style={{ marginTop: "24px", fontSize: "12px", color: "#71738A" }}>
+                今日新增0个小游戏
+                {/* <Image src={addicon} alt="add_icon" width="44px" height="44px" onClick={() => window.location.href = "https://www.wenjuan.com/s/UZBZJvEOdvM/#%E3%80%8A%E5%B0%8F%E6%B8%B8%E6%88%8F%E6%8E%A8%E8%8D%90%E3%80%8B"} /> */}
             </div>
         </div>
     )
