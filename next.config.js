35667510d0a435bafae09054b21d0cbffdc86262
Kuss <kussssss@outlook.com>
2022-11-13 18:31:29 +0800

    fix: next export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // NOTE: https://stackoverflow.com/a/70047180
+  images: {
+    loader: 'akamai',
+    path: '',
+  }
 }
 
 module.exports = nextConfig
